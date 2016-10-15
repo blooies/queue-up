6a6351fe5d985de872f4911053d0ded21dcff787
blooies <joy28@msn.com>
2016-10-15 16:23:40 -0400

    changed name of twilio controller

--- server/twilio_controller.js
+++ server/twilio_controller.js
@@ -2,17 +2,20 @@ var twilio = require("../node_modules/twilio/lib");
 var accountSid = 'AC55e374775b226d4fb6da4e8158d71c9f'; // Your Account SID from www.twilio.com/console
 var authToken = 'b667a3a7c7fe347a962b649ea9bbb195';   // Your Auth Token from www.twilio.com/console
 var client = new twilio.RestClient(accountSid, authToken);
-const TWILIO_NUMBER = "+15168304800";
+const twilioNumber = "+15168304800";
 
 TwilioController = {
-    sendTextMessage: function(number) {
+    sendTextMessage: function(number, message) {
+        console.log("inside twilio controller", number, message)
+        number = "+1" + number;
         client.messages.create({
-            body: 'TEXT MESSAGE FROM APP!!!!',
+            body: message,
             to: number,  // Text this number
-            from: TWILIO_NUMBER, // From a valid Twilio number
+            from: twilioNumber // From a valid Twilio number
         }, function(err, message) {
             console.log(message);
-            console.log(err)
+            if (err) console.log("ERROR", err);
+            return message.sid;
         });
     }
 }
