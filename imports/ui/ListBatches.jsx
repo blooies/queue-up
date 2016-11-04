6a6ca92e6a2fd8c2c6d69097b0f1047493935db0
blooies <joy28@msn.com>
2016-11-04 14:48:44 -0400

    added list batches component

---
+++ imports/ui/ListBatches.jsx
@@ -0,0 +1,66 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+
+import { browserHistory } from 'react-router';
+
+export default class ListBatches extends Component {
+    constructor(props) {
+        super(props);
+
+        this.getMessage = this.getMessage.bind(this);
+        this.renderBatches = this.renderBatches.bind(this);
+    }
+
+    getTimeOverOrToSpare(lastBatch) {
+        var plannedEndTime = lastBatch.endTimeOriginal;
+        var lastBatchEndTime = lastBatch.endTime;
+        var laterTime = plannedEndTime;
+        var earlierTime = lastBatchEndTime;
+
+        if (lastBatch.overtime) {
+            laterTime = lastBatchEndTime;
+            earlierTime = plannedEndTime;
+        }
+
+        var hours = moment.duration(laterTime.diff(earlierTime)).hours();
+        var minutes = moment.duration(laterTime.diff(earlierTime)).minutes();
+        var time;
+        if (hours) {
+            time = hours + ' hours and ' + minutes + ' minutes';
+        } else if (minutes) {
+            time = minutes + ' minutes';
+        }
+
+        return time;
+    }
+
+    getMessage() {
+        var message = '';
+        var lastBatch = this.props.batches[this.props.batches.length - 1];
+        if (lastBatch.overtime) {
+            message += 'You are' + {this.getTimeOverOrToSpare(lastBatch)} + 'over time.';
+        } else {
+            message += 'Perfect! You have' + {this.getTimeOverOrToSpare(lastBatch)} + ' to spare.';
+        }
+        message +=  'End time will be' + {lastBatch.endTime};
+
+        return message;
+    }
+
+    renderBatches() {
+        return this.props.batches.map((batch) => (
+             <Batch
+                key={batch}
+                batch={batch}
+            />
+        ))
+    }
+
+    render() {
+        <div>
+            {this.renderBatches}
+            <button>Edit</button>
+            <button>Confirm</button>
+        </div>
+    }
+}
